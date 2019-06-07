70ed37582b30dd6b67e367d6f96f5ec5a5334f9c
ianmdurrant <ianmdurrant@gmail.com>
2019-06-07 14:39:24 +0100

    initial commit

---
+++ app/webpacker/packs/application.js
@@ -0,0 +1,78 @@
+/* eslint no-console:0 */
+
+import Rails from 'rails-ujs';
+import $ from 'jquery';
+import Turbolinks from 'turbolinks';
+import Foundation from 'foundation-sites';
+import * as ActiveStorage from 'activestorage';
+
+// Javascript
+import 'owl.carousel';
+
+// Styles
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import '../application.scss';
+
+// Images
+import 'images/facebook.svg';
+import 'images/instagram.svg';
+import 'images/twitter.svg';
+import 'images/facebook-white.svg';
+import 'images/instagram-white.svg';
+import 'images/twitter-white.svg';
+import 'images/logo-full.svg';
+import 'images/logo.svg';
+import 'images/arrow-right.svg';
+import 'images/arrow-left.svg';
+
+
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+Foundation.addToJquery($);
+
+document.addEventListener('turbolinks:load', function () {
+  $(document).foundation();
+});
+
+$(document).on("turbolinks:load", function() {
+  $(document).foundation();
+
+  setTimeout(function(){
+    if ($('#slider.owl-carousel').length > 0) {
+      $('#slider').owlCarousel({
+        items: 3,
+        autoplayHoverPause: true,
+        loop: true,
+        margin: 0,
+        autoHeight: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        navText: ['next', 'prev'],
+        responsive: {
+          0: {
+            items: 1
+          },
+          640: {
+            items: 3
+          }
+        }
+      });
+    }
+    if ($('#testimonialCarousel').length) {
+      $('#testimonialCarousel').owlCarousel({
+        autoplay: true,
+        autoplayHoverPause: true,
+        dots: false,
+        nav: true,
+        navText: ['&nbsp;', '&nbsp;'],
+        items: 1,
+        loop: true,
+        margin: 0,
+      })
+    }
+
+  }, 500);
+});
---
+++ config/initializers/themes/default.rb
@@ -0,0 +1,199 @@
+::Spina::Theme.register do |theme|
+  theme.name = 'default'
+  theme.title = 'Default Theme'
+
+  theme.page_parts = [
+    { name: 'text', title: 'Text', partable_type: 'Spina::Text' },
+    { name: 'image', title: 'Image', partable_type: 'Spina::Image' },
+    { name: 'image_collection', title: 'Image Collection', partable_type: 'Spina::ImageCollection' },
+    { name: 'banner', title: 'Banner Image', partable_type: 'Spina::Image' },
+    { name: 'banner_title', title: 'Banner Title', partable_type: 'Spina::Text' },
+    { name: 'banner_text', title: 'Banner Text', partable_type: 'Spina::Text' },
+    { name: 'testimonials', title: 'Testimonials', partable_type: 'Spina::Structure' }
+  ]
+
+  theme.view_templates = [{
+    name: 'homepage',
+    title: 'Homepage',
+    page_parts: ['banner', 'banner_title', 'text', 'image_collection']
+  }, {
+    name: 'show',
+    title: 'Default',
+    page_parts: ['text', 'image']
+  }, {
+    name: 'contact',
+    title: 'Contact',
+    page_parts: ['image', 'text']
+  }]
+
+  theme.custom_pages = [{
+    name: 'homepage',
+    title: 'Homepage',
+    deletable: false,
+    view_template: 'homepage'
+  }]
+
+  theme.navigations = [{
+    name: 'main',
+    label: 'Main navigation',
+    auto_add_pages: true
+  }, {
+    name: 'footer_menu',
+    label: 'Footer Menu',
+    auto_add_pages: false
+  }]
+
+  theme.structures = [{
+    name: 'testimonials',
+    structure_parts: [{
+      name:           'author',
+      title:          'Author',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'location',
+      title:          'Location',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'testimonial',
+      title:          'Testimonial',
+      partable_type:  'Spina::Text'
+    }]
+  },{
+    name: 'patrons',
+    structure_parts: [{
+      name:           'name',
+      title:          'Name',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'quote',
+      title:          'Quote',
+      partable_type:  'Spina::Text'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  },{
+    name: 'facilities',
+    structure_parts: [{
+      name:           'title',
+      title:          'Title',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'text',
+      title:          'Text',
+      partable_type:  'Spina::Text'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  },{
+    name: 'course_detail',
+    structure_parts: [{
+      name:           'title',
+      title:          'Title',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'text',
+      title:          'Text',
+      partable_type:  'Spina::Text'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  },{
+    name: 'directors',
+    structure_parts: [{
+      name:           'name',
+      title:          'Name',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'position',
+      title:          'Position',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  },{
+    name: 'music_tutors',
+    structure_parts: [{
+      name:           'name',
+      title:          'Name',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'position',
+      title:          'Position',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  },{
+    name: 'dance_tutors',
+    structure_parts: [{
+      name:           'name',
+      title:          'Name',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'position',
+      title:          'Position',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  },{
+    name: 'coaches',
+    structure_parts: [{
+      name:           'name',
+      title:          'Name',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'position',
+      title:          'Position',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  },{
+    name: 'guest_tutors',
+    structure_parts: [{
+      name:           'name',
+      title:          'Name',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'position',
+      title:          'Position',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  },{
+    name: 'admin',
+    structure_parts: [{
+      name:           'name',
+      title:          'Name',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'position',
+      title:          'Position',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'picture',
+      title:          'Picture',
+      partable_type:  'Spina::Image'
+    }]
+  }]
+
+
+end
